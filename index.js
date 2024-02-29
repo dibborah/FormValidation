da572aff6077198a202e0c6e9a81785ff1fa45a5
dibborah <dibyajyotiborah469@gmail.com>
2024-03-01 01:53:03 +0530

    completed validateEmail,  validateMessage and validateForm which ensures that is any Error left in wny field before submission

--- index.js
+++ index.js
@@ -37,3 +37,47 @@ function validatePhone (){
     phoneError.innerHTML = "valid";
     return true;
 }
+
+function validateEmail() {
+    const email = document.getElementById('contact-email').value;
+
+    if(email.length === 0){
+      emailError.innerHTML = 'Email is required';
+      return false;
+    }
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML = "Enter a valid email"
+        return false
+    }
+    emailError.innerHTML = "valid";
+    return true;
+}
+
+function validateMessage() {
+    const message = document.getElementById('contact-message').value;
+
+    const required = 30;
+    let left = required - message.length;
+
+    if(message.length === 0){
+        messageError.innerHTML = "Message is required";
+        return false;
+    }
+    if(left > 0){
+        messageError.innerHTML = `${left} word is required`
+        return false;
+    }
+    messageError.innerHTML = "valid";
+    return true;
+}
+
+function validateForm() {
+    if(!validateName() || !validatePhone() || !validateEmail() || !validateMessage()){
+        SubmitError.style.display = "block";// block makes an element visible taking full width of the container
+        SubmitError.innerHTML = "Please fix error";
+        setTimeout(() => {
+            SubmitError.style.display = "none";    
+        },2000)
+        return false;        
+    }
+}
