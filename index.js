ad6a7453c83a512c41dbf5767bb5999fca499b10
dibborah <dibyajyotiborah469@gmail.com>
2024-03-01 01:04:37 +0530

    validation in name and phone field added

--- index.js
+++ index.js
@@ -3,3 +3,37 @@ const phoneError = document.getElementById("phone-error");
 const emailError = document.getElementById("email-error");
 const messageError = document.getElementById("message-error");
 const SubmitError = document.getElementById("submit-error");
+
+function validateName() {
+    const name = document.getElementById("contact-name").value;
+
+    if(name.length === 0 ) {
+        nameError.innerHTML = "Name is required";
+        return false;
+    }
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = "Enter Full Name"
+        return false;
+    }
+    nameError.innerHTML = 'valid';
+    return true;
+}
+
+function validatePhone (){
+    const phone = document.getElementById("contact-phone").value;
+
+    if(phone.length === 0){
+        phoneError.innerHTML = "Phone number is required";
+        return false
+    }
+    if(phone.length !== 10){
+        phoneError.innerHTML = "Email must be of 10 digits";
+        return false;
+    }
+    if(!phone.match(/^[0-9]{10}$/)){
+        phoneError.innerHTML = "Only digits please";
+        return false;
+    }
+    phoneError.innerHTML = "valid";
+    return true;
+}
